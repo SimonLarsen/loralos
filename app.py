28d12a1cea149dce17ea208d450fcb1a14b785d0
Simon Jonas Larsen <SLN@energinet.dk>
2022-03-11 10:22:31 +0100

    Added cross section plot

--- app.py
+++ app.py
@@ -45,49 +45,44 @@ photo = WMSImage(
 )
 
 
-
 @cache.memoize(timeout=600)
-def generate_data(lon1, lat1, lon2, lat2, frequency, spm, compute_3d=False, padding=None):
+def generate_data(lon1, lat1, lon2, lat2, frequency, spm, padding):
     geod = pyproj.Geod(ellps="clrk66")
     azi1, azi2, dist = geod.inv(lon1, lat1, lon2, lat2)
+
+    max_r = fresnel_zone_radius(dist / 2.0, dist / 2.0, frequency) + 2.0 * padding
     npts_x = round(dist * spm)
+    npts_y = int(round(max_r * spm * 0.5) * 2 + 1)
+
+    out_lon = []
+    out_lat = []
+    out_d1 = []
+    out_offset = []
+
     inter = geod.inv_intermediate(lon1, lat1, lon2, lat2, npts=npts_x, initial_idx=0, terminus_idx=0)
 
-    if not compute_3d:
-        npts_y = 1
+    for ilon, ilat, in zip(inter.lons, inter.lats):
+        azi_fwd, azi_bwd, d1 = geod.inv(lon1, lat1, ilon, ilat)
+        flon1, flat1, faz1 = geod.fwd(ilon, ilat, azi_bwd - 90.0, max_r)
+        flon2, flat2, faz2 = geod.fwd(ilon, ilat, azi_bwd + 90.0, max_r)
 
-        out_lon = inter.lons
-        out_lat = inter.lats
-        out_d1 = np.linspace(0, dist, npts_x)
-        out_offset = None
-    else:
-        out_lon = []
-        out_lat = []
-        out_d1 = []
-        out_offset = []
+        rinter = geod.inv_intermediate(
+            flon1, flat1, flon2, flat2,
+            npts=npts_y,
+            initial_idx=0,
+            terminus_idx=0,
+        )
 
-        max_r = fresnel_zone_radius(dist / 2.0, dist / 2.0, frequency) + 2.0 * padding
-        npts_y = int(round(max_r * spm * 0.5) * 2 + 1)
-
-        for ilon, ilat, in zip(inter.lons, inter.lats):
-            azi_fwd, azi_bwd, d1 = geod.inv(lon1, lat1, ilon, ilat)
-            flon1, flat1, faz1 = geod.fwd(ilon, ilat, azi_bwd - 90.0, max_r)
-            flon2, flat2, faz2 = geod.fwd(ilon, ilat, azi_bwd + 90.0, max_r)
-
-            rinter = geod.inv_intermediate(
-                flon1, flat1, flon2, flat2,
-                npts=npts_y,
-                initial_idx=0,
-                terminus_idx=0,
-            )
-
-            out_lon.extend(rinter.lons)
-            out_lat.extend(rinter.lats)
-            out_d1.extend(np.repeat(d1, npts_y))
-            out_offset.extend(np.linspace(-max_r, max_r, npts_y))
+        out_lon.extend(rinter.lons)
+        out_lat.extend(rinter.lats)
+        out_d1.extend(np.repeat(d1, npts_y))
+        out_offset.extend(np.linspace(-max_r, max_r, npts_y))
 
     out_height = [heightmap.get_height(lon, lat) for lon, lat in zip(out_lon, out_lat)]
     out_color = [photo.get_pixel(lon, lat) for lon, lat in zip(out_lon, out_lat)]
+
+    height_start = out_height[int(npts_y) // 2]
+    height_end = out_height[int((npts_x - 1) * npts_y + npts_y // 2)]
 
     return dict(
         dist=dist,
@@ -98,7 +93,9 @@ def generate_data(lon1, lat1, lon2, lat2, frequency, spm, compute_3d=False, padd
         d1=out_d1,
         offset=out_offset,
         height=out_height,
-        color=out_color
+        color=out_color,
+        height_start=height_start,
+        height_end=height_end
     )
 
 
@@ -170,7 +167,7 @@ navbar = dbc.NavbarSimple(
 )
 
 options_stations = [dict(label=id, value=id) for id in stations.station]
-sidebar = html.Div(
+sidebar = dbc.Form(
     [
         html.Div([
             dbc.Label("Gateway location", html_for="gateway_id"),
@@ -185,7 +182,7 @@ sidebar = html.Div(
             for args in numeric_inputs
         ]),
         dbc.Switch(id="enable-3d-graph", label="Generate 3D terrain", value=True),
-        dbc.Button("Submit", id="submit", className="mt-3"),
+        dbc.Button("Submit", id="submit", type="submit", className="mt-3"),
     ],
     className="mt-3"
 ),
@@ -196,9 +193,24 @@ container = dbc.Container(
             dbc.Col(sidebar, md=3, lg=2),
             dbc.Col(
                 [
-                    html.H2("Height curve"),
-                    html.Div(dcc.Loading(dcc.Graph(id="graph-2d", figure=placeholder_figure("No results computed yet"))), className="border mb-3"),
-                    html.H2("3D terrain"),
+                    dbc.Row([
+                        dbc.Col([
+                            html.H3("Height curve"),
+                            html.Div(
+                                dcc.Loading(dcc.Graph(id="graph-2d", figure=placeholder_figure("No results computed yet", 250))),
+                                className="border"
+                            ),
+                        ], md=9),
+                        dbc.Col([
+                            html.H3("Cross section"),
+                            html.Div(
+                                dcc.Loading(dcc.Graph(id="graph-cross", figure=placeholder_figure("Hover over height curve to show cross section", 250))),
+                                className="border"
+                            ),
+                            html.P(id="test")
+                        ], md=3)
+                    ], className="mb-3"),
+                    html.H3("3D terrain"),
                     html.Div(dcc.Loading(dcc.Graph(id="graph-3d", figure=placeholder_figure("No results computed yet"))), className="border")
                 ],
                 md=9,
@@ -221,7 +233,8 @@ app.layout = html.Div([navbar, container])
     State("frequency", "value"),
     State("gateway_height", "value"),
     State("node_height", "value"),
-    State("spm", "value")
+    State("spm", "value"),
+    State("padding", "value")
 )
 def update_graph_2d(
     n_clicks: int,
@@ -230,29 +243,54 @@ def update_graph_2d(
     frequency: float,
     gateway_height: float,
     node_height: float,
-    spm: float
+    spm: float,
+    padding: float
 ):
     if n_clicks is None or n_clicks == 0:
         return placeholder_figure("This shouldn't happen. :(")
-    
+
     # Get location coordinates
     lon1, lat1 = stations.query("station == @gateway_id").iloc[0][["lon", "lat"]]
     lon2, lat2 = stations.query("station == @node_id").iloc[0][["lon", "lat"]]
 
-    result = generate_data(lon1, lat1, lon2, lat2, frequency, spm, compute_3d=False)
-    
+    result = generate_data(lon1, lat1, lon2, lat2, frequency, spm, padding)
+    indices = range(int(result["npts_y"] // 2), result["npts_x"]*result["npts_y"], result["npts_y"])
+    getter = itemgetter(*indices)
+    # TODO: Replace getter with slices
+
+    d1 = getter(result["d1"])
+    height = getter(result["height"])
+
     figure = go.Figure()
     figure.add_trace(
         go.Scatter(
-            x=result["d1"],
-            y=result["height"],
+            x=d1,
+            y=height,
+            customdata=d1,
+            name="Surface height",
             mode="lines"
         )
     )
 
+    height_start = result["height_start"] + gateway_height
+    height_end = result["height_end"] + node_height
+
+    fresnel_npts_x = int(config["fresnel"]["steps_x"])
+    d1 = np.linspace(0, result["dist"], fresnel_npts_x)
+    h = np.linspace(height_start, height_end, fresnel_npts_x)
+    r = fresnel_zone_radius(d1, result["dist"] - d1, frequency)
+
+    figure.add_trace(
+        go.Scatter(x=d1, y=h-r, name="Fresnel zone (lower)", mode="lines")
+    )
+    figure.add_trace(
+        go.Scatter(x=d1, y=h+r, name="Fresnel zone (upper)", mode="lines")
+    )
+
     figure.update_layout(
         height=250,
-        margin=dict(t=0, r=0, b=0, l=0)
+        margin=dict(t=0, r=0, b=0, l=0),
+        showlegend=False
     )
     return figure
 
@@ -290,7 +328,7 @@ def update_graph_3d(
     lon1, lat1 = stations.query("station == @gateway_id").iloc[0][["lon", "lat"]]
     lon2, lat2 = stations.query("station == @node_id").iloc[0][["lon", "lat"]]
 
-    result = generate_data(lon1, lat1, lon2, lat2, frequency, spm, compute_3d=True, padding=padding)
+    result = generate_data(lon1, lat1, lon2, lat2, frequency, spm, padding)
     dist = result["dist"]
     t1, t2, t3 = generate_mesh_indices(result["npts_x"], result["npts_y"])
     color_rgb = ["#{:02x}{:02x}{:02x}".format(*c) for c in result["color"]]
@@ -313,12 +351,11 @@ def update_graph_3d(
         )
     )
 
-    height_start = heightmap.get_height(lon1, lat1) + gateway_height
-    height_end = heightmap.get_height(lon2, lat2) + node_height
-
     x, y, z = [], [], []
-    fresnel_npts_x = round(dist / 50)
-    fresnel_npts_y = 12
+    fresnel_npts_x = int(config["fresnel"]["steps_x"])
+    fresnel_npts_y = int(config["fresnel"]["steps_y"])
+    height_start = result["height_start"] + gateway_height
+    height_end = result["height_end"] + node_height
     for h, d1 in zip(
         np.linspace(height_start, height_end, fresnel_npts_x),
         np.linspace(0, dist, fresnel_npts_x)
@@ -347,10 +384,72 @@ def update_graph_3d(
     return figure
 
 
-def main():
-    cache.clear()
-    app.run_server(debug=True)
+@app.callback(
+    Output("graph-cross", "figure"),
+    Input("graph-2d", "hoverData"),
+    State("gateway_id", "value"),
+    State("node_id", "value"),
+    State("frequency", "value"),
+    State("gateway_height", "value"),
+    State("node_height", "value"),
+    State("spm", "value"),
+    State("padding", "value"),
+)
+def update_graph_cross(
+    hoverData,
+    gateway_id: str,
+    node_id: str,
+    frequency: float,
+    gateway_height: float,
+    node_height: float,
+    spm: float,
+    padding: float,
+):
+    if hoverData["points"][0]["curveNumber"] != 0:
+        raise PreventUpdate
+
+    lon1, lat1 = stations.query("station == @gateway_id").iloc[0][["lon", "lat"]]
+    lon2, lat2 = stations.query("station == @node_id").iloc[0][["lon", "lat"]]
+    result = generate_data(lon1, lat1, lon2, lat2, frequency, spm, padding)
+
+    point_index = hoverData["points"][0]["pointIndex"]
+    data_start = result["npts_y"] * point_index
+
+    d1 = hoverData["points"][0]["customdata"]
+    r = fresnel_zone_radius(d1, result["dist"] - d1, frequency)
+    height = result["height"][data_start:data_start+result["npts_y"]]
+    offset = result["offset"][data_start:data_start+result["npts_y"]]
+
+    t = d1 / result["dist"]
+    height_start = result["height_start"] + gateway_height
+    height_end = result["height_end"] + node_height
+    los_height = height_end * t + (1.0 - t) * height_start
+
+    figure = go.Figure()
+    figure.add_trace(
+        go.Scatter(
+            x=offset,
+            y=height,
+            mode="lines"
+        )
+    )
+    figure.add_shape(
+        type="circle",
+        xref="x",
+        yref="y",
+        x0=-r, y0=los_height-r, x1=r, y1=los_height+r
+    )
+    figure.update_layout(
+        height=250,
+        margin=dict(t=0, r=0, b=0, l=0),
+    )
+    figure.update_yaxes(
+        scaleanchor="x",
+        scaleratio=1
+    )
+    return figure
 
 
 if __name__ == "__main__":
-    main()
+    cache.clear()
+    app.run_server(debug=True)
